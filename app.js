0d7959024c419bafd4aa6ba1072c2562b757abb4
MIM II K <155126757+MIMIIKK@users.noreply.github.com>
2024-06-14 07:39:43 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const expressHbs = require('express-handlebars')
 
@@ -24,31 +23,3 @@ const port = 4444;
 
 app.listen(port, () =>{
     console.log(`running at http://localhost:${port}`);
-=======
-const express = require('express')
-const expressHbs = require('express-handlebars')
-
-const app = express();
-
-const hbs = expressHbs.create({
-    extname: ".hbs",
-    defaultLayout: "main.hbs",
-    layoutsDir: "views/layout"
-})
-
-//setting view engine
-app.set('view engine', 'hbs');
-
-app.use('/static', express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: true}))
-
-const home = require('./routes/index');
-//routing
-app.use('/', home);
-
-const port = 4444;
-
-app.listen(port, () =>{
-    console.log(`running at http://localhost:${port}`);
->>>>>>> 3cd97eeeb0f75cd705d9c76003bcd676dbfbfb0f
-})
